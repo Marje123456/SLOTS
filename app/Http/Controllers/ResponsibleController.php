3da2949d9a765d52c53edb9adc0caad1b3693156
Marje123456 <marjeiris.rodriguez@gmail.com>
2024-03-07 00:36:03 -0400

    migracion controlador y modelo de sujeto responsible

---
+++ app/Http/Controllers/ResponsibleController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Responsible;
+use Illuminate\Http\Request;
+
+class ResponsibleController extends Controller
+{
+    /**
+     * Display a listing of the resource. Mostrar un listado del recurso. Leer todos los registros / Mostrar
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource. Muestra el formulario para crear un nuevo recurso. Formulario para nuevo registro
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage. Almacena un recurso recién creado en el almacén. Guardar en sb el nuevo registro
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource. Muestra el recurso especificado. Visualizar un registro especifico
+     */
+    public function show(Responsible $responsible)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource. Mostrar el formulario para editar el recurso especificado. 
+     */
+    public function edit(Responsible $responsible)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage. Actualiza el recurso especificado en el almacenamiento.
+     */
+    public function update(Request $request, Responsible $responsible)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage. Elimina el recurso especificado del almacenamiento.
+     */
+    public function destroy(Responsible $responsible)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_03_07_040143_create_responsibles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('responsibles', function (Blueprint $table) {
+            $table->id();
+            $table->string('idc', 15)->unique();
+            $table->string('names', 50);
+            $table->string('last_names', 50);
+            $table->string('phone', 20)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('responsibles');
+    }
+};
